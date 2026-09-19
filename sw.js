7d4e8d4512e6b0ab2aa555f720d8ab037137f9e3
Eric Jenkins <ericdjenkins@gmail.com>
2026-09-19 09:50:55 -0700

    Use versioned profile photo on app home screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='eric-jenkins-workout-v3';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icons/icon.svg','icons/icon-192.png','icons/icon-512.png','images/eric-lean-bucket-hat.png'];
+const CACHE='eric-jenkins-workout-v4';
+const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icons/icon.svg','icons/icon-192.png','icons/icon-512.png','images/eric-lean-bucket-hat-v2.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('index.html'))))});
